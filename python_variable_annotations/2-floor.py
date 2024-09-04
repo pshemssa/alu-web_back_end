1e78601bbb8fad3603511664bdcc861bc6825a16
pshemssa <122387932+pshemssa@users.noreply.github.com>
2024-09-04 15:04:51 +0000

    annotation

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -7,4 +7,3 @@ def floor(n: float) -> int:
     '''returns the floor of a floating-point number.
     '''
     return int(n)
-    
--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -7,4 +7,3 @@ def to_str(n: float) -> str:
     '''Converts a floating-point number to a string.
     '''
     return str(n)
-    
