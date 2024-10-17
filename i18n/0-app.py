27850a30b3c601bf7294825c7aa08935f712e335
pshemssa <122387932+pshemssa@users.noreply.github.com>
2024-10-17 12:56:42 +0000

    flask

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -6,9 +6,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """index"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
