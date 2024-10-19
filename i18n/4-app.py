003844f53d221e0fd622687482f9c7ff912a7703
pshemssa <122387932+pshemssa@users.noreply.github.com>
2024-10-19 13:08:35 +0000

    force locale

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,41 +1,46 @@
 #!/usr/bin/env python3
-
-"""Basic Flask app"""
-
-from flask import Flask, render_template
+"""
+Flask app
+"""
+from flask import Flask, render_template, request
 from flask_babel import Babel
-
-app = Flask(__name__)
-babel = Babel(app)
 
 
 class Config:
-    """CONFIG CLASS"""
+    """
+    Config class
+    """
+    LANGUAGES = ['en', 'fr']
 
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-
-app.config.from_object(Config)
+app = Flask(__name__)
 app.url_map.strict_slashes = False
+app.config.from_object(Config)
+
+babel = Babel(app)
+Babel.default_locale = 'en'
+Babel.default_timezone = 'UTC'
 
 
 @app.route('/')
-def index():
-    """index"""
+def hello():
+    """ GET /
+    Return:
+      - Render template
+    """
     return render_template('4-index.html')
 
 
 @babel.localeselector
 def get_locale():
-    """GET LOCALE"""
-
+    """
+    Get locale from request
+    """
     locale = request.args.get('locale')
     if locale in Config.LANGUAGES:
         return locale
     return request.accept_languages.best_match(Config.LANGUAGES)
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
