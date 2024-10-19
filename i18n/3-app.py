c0e73c948364bf88756e6c412e8d4ca321ccf125
pshemssa <122387932+pshemssa@users.noreply.github.com>
2024-10-19 12:32:28 +0000

    Parametrize templates

---
+++ i18n/3-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""Basic Flask app"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """CONFIG CLASS"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """index"""
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """GET LOCALE"""
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
