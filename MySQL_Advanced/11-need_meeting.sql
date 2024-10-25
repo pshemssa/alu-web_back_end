aba355a98ec3cce8f620e81272a72cb0576e2d31
pshemssa <122387932+pshemssa@users.noreply.github.com>
2024-10-25 16:02:13 +0000

    mysql

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -1,4 +1,4 @@
 --that lists all students that have a score under 80 --
 CREATE VIEW need_meeting AS
 SELECT name FROM students WHERE score < 80
-    AND (students.last_meeting IS NULL OR students.last_meeting < SUBDATE(CURRENT_DATE(),INTERVAL -1 MONTH));
+AND (students.last_meeting IS NULL OR students.last_meeting < DATE_ADD(NOW(), INTERVAL -1 MONTH));
