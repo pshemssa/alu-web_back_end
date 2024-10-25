66b214bf3a6fad1209726d9f8337fd4916c72fb1
pshemssa <122387932+pshemssa@users.noreply.github.com>
2024-10-25 15:43:04 +0000

    mysql

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,16 @@
+-- a function thta divides the first by the second--
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT division
+BEGIN
+    DECLARE result FLOAT DEFAULT 0;
+
+    IF b != 0 THEN
+        SET result = a /b;
+END IF;
+RETURN result;
+
+END $$
+
+DELIMITER;
