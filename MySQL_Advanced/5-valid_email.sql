9f45a31e819af4228ab92b7f7d44d6911b162022
pshemssa <122387932+pshemssa@users.noreply.github.com>
2024-10-25 15:24:57 +0000

    mysql

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,5 +1,11 @@
 -- a reseting trigger --
+DELIMITER $$
+
 CREATE TRIGGER new_email BEFORE UPDATE ON users
 FOR EACH ROW IF OLD.email != NEW.email THEN
 	SET NEW.valid_email = 0;
     END IF;
+
+end$$
+
+DELIMITER;
