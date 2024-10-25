7ed078bf9eec15f8e618baa0cd7debfa5f2bea2e
pshemssa <122387932+pshemssa@users.noreply.github.com>
2024-10-25 13:42:51 +0000

    mysql

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,6 +1,8 @@
 -- ranking country of origin--
 
-import './metal_bands'
 
-select origin, SUM(fans) as nb_fans from metal_bands 
-GROUP BY origin ORDER BY nb_fans DESC;
+use metal_bands;
+select origin, SUM(fans) as nb_fans 
+from metal_bands 
+GROUP BY origin 
+ORDER BY nb_fans DESC;
